0358ebc2e5c8ff49b310a24dbfe1a8621f695c24
Damian Trybuszek <damian.trybuszek@gmail.com>
2021-04-28 23:33:26 +0200

    Functions to create and print the table implemented.

--- engine.py
+++ engine.py
@@ -1,15 +1,34 @@
+import tcod
+
 def create_board(width, height):
-    '''
-    Creates a new game board based on input parameters.
 
-    Args:
-    int: The width of the board
-    int: The height of the board
+    board = []
 
-    Returns:
-    list: Game board
-    '''
-    pass
+    for i in range(height):
+        temp_table = []
+        for j in range(width):
+            if i == 3 and j == width-1:
+                temp_table.append(" ")
+            elif j == 0 or j == width -1:
+                temp_table.append("#")
+            elif i == 0 or i == height -1:
+                temp_table.append("#")
+            else:
+                temp_table.append(" ")
+        board.append(temp_table)
+
+    return board
+
+
+
+# print(create_board(10,5))
+
+def print_board(board):
+    for element in board:
+        print("".join(element))
+
+print_board(create_board(25,10))
+
 
 
 def put_player_on_board(board, player):
