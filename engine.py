7c7981b73eef28f27cd01e1ed905456f5be1df2b
Damian Trybuszek <damian.trybuszek@gmail.com>
2021-05-03 14:17:17 +0200

    Paths implemented

--- engine.py
+++ engine.py
@@ -14,9 +14,9 @@ def create_board_level_1(width, height):
             if i == 3 and j == width-1:
                 board[i][j]=main.GATE_ICON
             elif j == 0 or j == width -1:
-                board[i][j]="#"
+                board[i][j]=main.WALL_ICON
             elif i == 0 or i == height -1:
-                board[i][j]="#"
+                board[i][j]=main.WALL_ICON
 
     for i in range(height+2):
         for j in range(width-4):
@@ -25,9 +25,9 @@ def create_board_level_1(width, height):
             elif i == 4 and j == 0:
                 board[i][j+30]=main.GATE_ICON
             elif j == 0 or j == width - 4 -1:
-                board[i][j+30]="#"
+                board[i][j+30]=main.WALL_ICON
             elif i == 0 or i == height +2 - 1:
-                board[i][j+30]="#"
+                board[i][j+30]=main.WALL_ICON
 
     for i in range(height-2):
         for j in range(width+2):
@@ -36,9 +36,9 @@ def create_board_level_1(width, height):
             elif i == 3 and j == 0:
                 board[i+18][j+34]=main.GATE_ICON
             elif j == 0 or j == width +2 -1:
-                board[i+18][j+34]="#"
+                board[i+18][j+34]=main.WALL_ICON
             elif i == 0 or i == height - 2 - 1:
-                board[i+18][j+34]="#"
+                board[i+18][j+34]=main.WALL_ICON
 
     for i in range(height):
         for j in range(width-4):
@@ -47,9 +47,65 @@ def create_board_level_1(width, height):
             elif i == height/2 and j == (width- 4)/2:
                 board[i+20][j+10]=main.GATE_TO_UPPER_LEVEL
             elif j == 0 or j == width - 4 -1:
-                board[i+20][j+10]="#"
+                board[i+20][j+10]=main.WALL_ICON
             elif i == 0 or i == height - 1:
-                board[i+20][j+10]="#"
+                board[i+20][j+10]=main.WALL_ICON
+
+    gate_coords = []
+
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            if board[i][j] == main.GATE_ICON:
+                gate_coords.append((i,j))
+
+    distance_1 = gate_coords[1][1] - gate_coords[0][1]
+    distance_2 = gate_coords[3][1] - gate_coords[2][1]
+    distance_3 = gate_coords[3][0] - gate_coords[2][0]
+    distance_4 = distance_2 + 3 -  (gate_coords[3][1] - gate_coords[2][1])
+    distance_5 = gate_coords[4][1] - gate_coords[5][1]
+    distance_6 = gate_coords[5][0] - gate_coords[4][0]
+
+
+
+
+    for i in range(distance_1-1):
+        if i <= distance_1//2:
+            height = gate_coords[0][0] 
+            width = gate_coords[0][1] +1 
+            board[height][width+i] = main.PATH_ICON
+        if i >= distance_1 // 2:
+            height = gate_coords[0][0] + 1
+            width = gate_coords[0][1] + 1 
+            board[height][width+i] = main.PATH_ICON
+        
+    for i in range(distance_2+3):
+        if i <= distance_2 + 3:
+            height = gate_coords[2][0] 
+            width = gate_coords[2][1] +1 
+            board[height][width+i] = main.PATH_ICON
+        if i == distance_2 + 2:
+            for j in range(distance_3+1):
+                board[height+j][width + i] = main.PATH_ICON
+
+    for i in range(distance_4-1):
+        height = gate_coords[3][0] 
+        width = gate_coords[3][1] +1 
+        board[height][width+i] = main.PATH_ICON
+
+    for i in range(distance_5):
+        if i <= distance_5//2:
+            height = gate_coords[5][0] 
+            width = gate_coords[5][1] +1 
+            board[height][width+i] = main.PATH_ICON
+        if i == distance_5 // 2:
+            for j in range(distance_6):
+                board[height-j][width + i] = main.PATH_ICON
+    
+    for i in range(distance_5-1):
+        if i >= distance_5//2:
+            height = gate_coords[5][0] - distance_6 
+            width = gate_coords[5][1] +1
+            board[height][width+i] = main.PATH_ICON
 
     return board
 
@@ -67,18 +123,19 @@ def put_player_on_board(board, player, move):
         for j in range(len(board[i])):
             if board[i][j] == main.GATE_ICON:
                 gate_coords.append((i,j))
+
                 
 
-    if move.lower() == "w" and board[height-1][width] not in ["#", main.GATE_ICON]:
+    if move.lower() == "w" and board[height-1][width] not in [main.WALL_ICON, main.GATE_ICON]:
         board[height][width] = " "
         height -=1
-    elif move.lower() == "a" and board[height][width-1] not in ["#", main.GATE_ICON]:
+    elif move.lower() == "a" and board[height][width-1] not in [main.WALL_ICON, main.GATE_ICON]:
         board[height][width] = " "
         width -= 1
-    elif move.lower() == "s" and board[height+1][width] not in ["#", main.GATE_ICON]:
+    elif move.lower() == "s" and board[height+1][width] not in [main.WALL_ICON, main.GATE_ICON]:
         board[height][width] = " "
         height += 1
-    elif move.lower() == "d" and board[height][width+1] not in ["#", main.GATE_ICON]:
+    elif move.lower() == "d" and board[height][width+1] not in [main.WALL_ICON, main.GATE_ICON]:
         board[height][width] = " "
         width += 1
     
