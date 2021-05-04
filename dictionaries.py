4955612428353efcca25294cf3b44e462fb4e561
gorzalakamil <kamilgorzala.kontakt@gmail.com>
2021-05-04 18:48:05 +0200

    add new faile

---
+++ dictionaries.py
@@ -0,0 +1,51 @@
+items = {
+    'sword':{
+        "icon": "S",
+        "atack": 2,
+        "def": 0,
+        'position_x': 9,
+        'position_y': 7    
+    },
+    'armor':{
+        "icon" : "A",
+        "atack": 0,
+        "def": 3,
+        'position_x': 6,
+        'position_y': 8,
+    },
+    'shield':{  
+        "icon": "S",
+        "atack": 0,
+        "def": 3,
+        'position_x': 7,
+        'position_y': 8
+    }
+}
+enemy = {
+    'monster':{
+        "icon": "M",
+        "hp": 5,
+        "atack": 2,
+        "def" : 1,
+        'position_x': 20,
+        'position_y': 25,
+    },
+    'boss':{
+        "icon": "B",
+        "hp": 10,
+        "atack": 8,
+        "def" : 3,
+        'position_x': 20,
+        'position_y': 25,
+}
+}
+player = {
+    "icon": "@",
+    "hp":20,
+    "atack": 4,
+    "def": 5,
+    'position_x': 5,
+    'position_y': 3,
+}
+
+inventory = {}
